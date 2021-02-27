b82d72cf6164be78ccb6a6930d7d3c40d6ff68c0
maaz1996 <maaz.abdullah111@gmail.com>
2021-02-27 17:26:16 +0530

    demo api working

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express=require("express");
 const app= express();
 const config= require ("./config")
-// app.use("/api/v1");
+ app.use("/",(req,res,next)=>{
+     res.status(200).send({
+         status:"200",
+         message: "API Req ok"
+     })
+ }
+ );
 
 app.listen(config["port"],()=>{
     console.log(`Server started on port :${config["port"]}`)
